015aa36875b18c6b09e501a4b54cb0f0062314e9
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-04-02 14:27:44 +0800

    Add html to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <div>
+        <h1>Welcome to the Dashboard App</h1>
+        <p>
+          Manage your tasks, track analytics, and stay organized with our
+          powerful dashboard.
+        </p>
+        <Link href="/dashboard">Go to Dashboard</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
