78774270fb94aef94a4c813eb83f8e6fa8497bdc
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-04-02 14:40:45 +0800

    style home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <h1>Welcome to the Dashboard App</h1>
-        <p>
-          Manage your tasks, track analytics, and stay organized with our
-          powerful dashboard.
-        </p>
-        <Link href="/dashboard">Go to Dashboard</Link>
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div>
+          <h1 className="text-5xl font-bold">Welcome to the Dashboard App</h1>
+          <p className="py-6">
+            Manage your tasks, track analytics, and stay organized with our
+            powerful dashboard.
+          </p>
+          <Link href="/dashboard" className="btn btn-primary rounded-2xl">
+            Go to Dashboard
+          </Link>
+        </div>
       </div>
     </div>
   );
