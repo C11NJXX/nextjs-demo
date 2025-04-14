8741f16c36216344feb58ddcaa2351ce89d6574c
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-04-14 10:33:24 +0800

    Update with dynamic routes

--- src/app/movies/[id]/route.ts
+++ src/app/movies/[id]/route.ts
@@ -10,3 +10,21 @@ export async function GET(
     ? Response.json(movie)
     : new Response("Movie not found", { status: 404 });
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: String } }
+) {
+  const { id } = await params;
+  const movieId = +id;
+  //find if movie-id exist
+  const movie = movies.find((m) => m.id === movieId);
+  if (!movie) {
+    return new Response("Movie not exists", { status: 404 });
+  }
+  //movie exists => find index and update movie
+  const updateMovie = await req.json();
+  const movieIndex = movies.findIndex((m) => m.id === movieId);
+  movies[movieIndex] = { ...movie, ...updateMovie };
+  return new Response(JSON.stringify(movies[movieIndex]), { status: 200 });
+}
